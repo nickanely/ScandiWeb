5855a1c7da89d03099e2b0e054475b0bdc1f95be
Nikoloz aneli <77580098+nickanely@users.noreply.github.com>
2023-02-09 14:41:32 +0400

    Add files via upload

---
+++ add_product.php
@@ -0,0 +1,32 @@
+<?php 
+// $servername = "localhost";
+// $username = "username";
+// $password = "password";
+// $dbname = "database_name";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+
+// $sku = $_POST["sku"];
+// $name = $_POST["name"];
+// $price = $_POST["price"];
+// $attribute = $_POST["attribute"];
+// $value = $_POST["value"];
+
+// $sql = "INSERT INTO products (sku, name, price, attribute, value)
+// VALUES ('$sku', '$name', '$price', '$attribute', '$value')";
+
+// if ($conn->query($sql) === TRUE) {
+//   header("Location: products_list.html");
+//   exit;
+// } else {
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+// }
+
+// $conn->close();
+?>
